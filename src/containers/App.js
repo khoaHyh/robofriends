863277a710e3471d9174e5a606dd208e635228dd
khoaHyh <khoahuynh.toronto@gmail.com>
2020-08-19 18:53:48 -0400

    changed promises to async/await

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,10 +13,18 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+  // old Promise 
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(response=> response.json())
+  //     .then(users => {this.setState({ robots: users})});
+  // }
+
+  async componentDidMount() {
+    const getUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+    const response = await getUsers.json();
+    const users = this.setState({ robots: response});
+    return users;
   }
 
   onSearchChange = (event) => {
